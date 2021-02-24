2c05675fe08cc836c4145c025f7d89cec6848b90
Uladzimir Atroshchanka <uladzimir_atroshchanka@epam.com>
2021-02-24 21:25:15 +0300

    fix: build

--- webpack-configs/webpack.prod.js
+++ webpack-configs/webpack.prod.js
@@ -1,17 +1,17 @@
-const path = require("path");
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  devtool: false,
-  output: {
-    path: path.resolve(__dirname, "../dist"),
-    publicPath: "/",
-    filename: "js/[name].[contenthash].bundle.js"
-  },
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000
-  }
+    devtool: false,
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: './',
+        filename: 'js/[name].[contenthash].bundle.js',
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
+    },
 });
